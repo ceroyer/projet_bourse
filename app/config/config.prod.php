b8b2055c1f30bc90a3c76bfc8b827417f4147abc
Mobandon <31806656+Mobandon@users.noreply.github.com>
2018-01-23 17:15:43 +0100

    change

--- app/config/config.prod.php
+++ app/config/config.prod.php
@@ -1,18 +1,22 @@
 <?php
 // Nom de la base de données
-define ('DB_NAME', 'mmiangouetuds');
+define ('DB_NAME', 'bo_bourse');
 // Utilisateur de la base de données SQL
-define ('DB_USER', 'mmiangouetuds');
+define ('DB_USER', 'user_bdd');
 // Mot de passe de la basse de données SQL
-define ('DB_PASSWORD', 'VSPDCgAfbTr7');
+define ('DB_PASSWORD', 'mdp_bdd');
 // Adresse de l'hébergement MySQL
 define ('DB_HOST', 'mmiangouetuds.mysql.db');
 // Jeu de caractères
 define ('DB_CHARSET', 'utf8');
 // Préfixe de table
-define('TABLE_PREFIX', 'dtwl_');
+define('TABLE_PREFIX', '');
 // Indique si le debuguage est activé : cible 'screen' ou 'console'
 define('DEBUG', true);
 define('DEBUG_TARGET', 'screen');
 
+<<<<<<< HEAD
 define('ROOT_URL', '/dev.bourse.fr');
+=======
+define('ROOT_URL', '/projet_bourse');
+>>>>>>> master
