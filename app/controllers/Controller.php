50e0837151a6b9f85df76658c02a2ec388561b58
Siloé <srabadan@etud.iut.local>
2018-01-30 15:28:01 +0100

    login blade

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -1,5 +1,5 @@
 <?php
-namespace Controllers;
+namespace controllers;
 
 class Controller {
 
--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -3,30 +3,33 @@
 namespace controllers;
 use Models\Tile;
 use Models\Admin;
+use Controllers\Controller;
 
 class LoginController extends Controller{
 
   // Accès page de connexion
   public function index(){
-    if (isset($_SESSION['login']))  //Lorsque l'utilisateur est deja connecté
-      {
-        redirect('/     '); // Accès à l'espace connecté
-      }
-
-    else { // Si pas connecté affichage de l'espace connexion
-      global $blade;
-      $logins = UsersDAO::getInstance()->getList();
-      echo $blade->render(
-      'login', // appel de la view
-      [
-        'logins' => $logins
-      ]);
+    
       }
 
   // 
   public function login(){
+        if (isset($_SESSION['login']))  //Lorsque l'utilisateur est deja connecté
+          {
+            redirect('/     '); // Accès à l'espace connecté
+          }
 
+        else { // Si pas connecté affichage de l'espace connexion
+          global $blade;
+          $logins = UsersDAO::getInstance()->getList();
+          echo $blade->render(
+          'login', // appel de la view
+          [
+            'logins' => $logins
+          ]);
   }
+
+}
 
 }
 
