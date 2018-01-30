6ae1427dd5eb8218f3fa089c39de4cc2ea4517ce
julietilatti <jtilatti@etud.iut.local>
2018-01-30 17:34:20 +0100

    ajout route bo

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,13 @@
 <?php
 $route->addRoute( 'GET'  , '/' ,         'LoginController@loginPage' );
+
+//Route backoffice
 $route->addRoute( 'GET'  , '/bo' ,       'IndexController@backofficeIndex' );
+$route->addRoute( 'GET'  , '/bo/delete' , 'IndexController@backofficeDelete' );
+$route->addRoute( 'GET'  , '/bo/upgrade' , 'IndexController@backofficeUpgrade' );
+
+
+
 $route->addRoute( 'GET'  , '/profile' ,  'IndexController@editProfile' );
 $route->addRoute( 'POST' , '/profile' ,  'IndexController@saveProfile' );
 $route->addRoute( 'GET'  , '/stats' ,    'IndexController@connectedPage' );
