3158235083872c40cc840572f3cf5e59bb0ac17b
Narghold <n4ghold87@gmail.com>
2018-01-30 17:07:00 +0100

    IndexController -> profile

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,14 +1,31 @@
 <?php
 
 namespace Controllers;
+use Models\Users.php;
 
 
 class IndexController extends Controller{
 
 function editProfile{
+  global $blade;
 
+  $id = $_GET['id'];
+  $user = Users::getInstance()->get($id);
+  echo $blade -> render('profile'['user' => $user]);
 }
 
+function saveProfile{
+  global $blade;
 
+  $id = $_POST['id'];
+  $user = Users::getInstance()->get($id);
+  if($_POST['password_old'] == $user['password'] && $_POST['password_new'] != $user['password'] && $user['password_new'] == $user['password_verif']){
+    $datas = ['email' => $_POST['email'] , 'password' => $_POST['password']];
+    User::getInstance()->edit($id , $datas);
+    redirect('/stats');
+  }else{
+    redirect('/');
+  }
+}
 }
 
