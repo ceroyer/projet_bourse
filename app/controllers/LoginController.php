2a39f5c8a1fb96048e004a364161b8f7e3e56e35
Mobandon <31806656+Mobandon@users.noreply.github.com>
2018-02-06 14:28:29 +0100

    Début d'afichage si l'utilisateur entre un mauvais mot de passe

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -10,21 +10,24 @@ class LoginController extends Controller{
 
   public function loginPage(){
     if (isset($_SESSION['login'])){  //Lorsque l'utilisateur est deja connecté
-      
+
       redirect('/stats'); // Accès à l'espace connecté
     }
 
     else { // Si pas connecté affichage de l'espace connexion
       global $blade;
       $logins = Users::getInstance()->getAll();
+      if (!isset($_SESSION['error'])){$_SESSION['error'] = false;} //si c'est vide, c'est faux
+      //dump($_SESSION['error']);die();
       echo $blade->render(
-      'login' // appel de la view
+      'login', // appel de la view
+      ['error' => $_SESSION['error'],]
       );
     }
   }
 
   public function login(){
-    
+
     $logins = Users::getInstance()->getAll();
 
 
@@ -32,12 +35,12 @@ class LoginController extends Controller{
 
       $loginconnect = $_POST['login']; // Récupération des variables
       $passwordconnect = sha1($_POST['password']); // Conversion en Sha1
-        
+
       foreach ($logins as $login) {
 
         if ($login['pseudo'] == $loginconnect AND $login['password'] == $passwordconnect ) {    // Si pseudo & mdp correct
 
-          $_SESSION['login']=$login['pseudo']; 
+          $_SESSION['login']=$login['pseudo']; //changer pour $login['id'] ?
           redirect('/stats'); // acces aux stats
         }
         else{
@@ -45,7 +48,8 @@ class LoginController extends Controller{
           $_POST=null; // Vider les champs & variables
           $loginconnect=null;
           $passwordconnect=null;
-          
+          $_SESSION['error'] = true;
+
           // Message erreur 'Pseudo ou mdp incorrects'
         }
       }
@@ -54,9 +58,9 @@ class LoginController extends Controller{
       redirect('/');
         // Afficher message erreur 'Champs vides'
     }
-      
-    
-    
+
+
+
 
 }
 
