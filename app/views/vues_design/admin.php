a5484fc4174b3ad1d7f49ba60fe1d5c2e3161e09
Noémie Pineaud <36069975+Ainaweh@users.noreply.github.com>
2018-02-07 18:04:19 +0100

    ajout page admin+maj css

---
+++ app/views/vues_design/admin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/css/header+footer+admin.css">
+  <title>Page administrateur</title>
+</head>
+<body>
+  <section id="espace_admin">
+    <label>Espace administrateur</label>
+    <div id="recherche">
+      <input type="texte" value="Rechercher un utilisateur" class="form-label">
+      <input type="button" value="Rechercher">
+    </div>
+    <div id="user">
+      <div id="infos">
+        <h3>Nom Utilisateur</h3>
+        <p>Inscrit le <strong>jj/mm/aaaa</strong></p>
+      </div>
+      <div id="boutons">
+        <a href="#">Voir le profil</a>
+        <a href="#">Supprimer</a>
+      </div>
+    </div>
+  </section>
+</body>
+</html>
