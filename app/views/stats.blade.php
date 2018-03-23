5f0d81b4ae39f6ee7e8390c6e77e0d268f5ea2fe
Noémie Pineaud <36069975+Ainaweh@users.noreply.github.com>
2018-03-23 14:51:16 +0100

    bem page stats

--- app/views/stats.blade.php
+++ app/views/stats.blade.php
@@ -32,73 +32,73 @@ Accueil
     <div class="cac">
       <div class="cac40">
         <h1>CAC 40</h1>
-    <table>
-      <thead>
-        <tr>
-          <td>Nom</td>
-          <td>ISIN</td>
-          <td>Ouverture</td>
-          <td>Fermeture</td>
-          <td>Min</td>
-          <td>Max</td>
-          <td>Variation</td>
-          <td>Fav</td>
-        </tr>
-       </thead>
-      @foreach ($actions as $action)
+        <table class="cac40__table">
+          <thead class="cac40__tableHead">
+            <tr>
+              <td>Nom</td>
+              <td>ISIN</td>
+              <td>Ouverture</td>
+              <td>Fermeture</td>
+              <td>Min</td>
+              <td>Max</td>
+              <td>Variation</td>
+              <td>Fav</td>
+            </tr>
+           </thead>
+          @foreach ($actions as $action)
 
-      <tbody>
-        <tr>
-          <td>{{ $action['nom'] }}</td>
-          <td>{{ $action['ISIN'] }}</td>
-          <td>{{ $action['ouverture'] }}</td>
-          <td>{{ $action['fermeture'] }}</td>
-          <td>{{ $action['bas'] }}</td>
-          <td>{{ $action['haut'] }}</td>
-          <td>{{ $action['variation'] }}
-            @if($action['variation']>= 0) <!-- fleche vers le haut -->
-              <svg style="fill:green"><polygon points='28,137.333 62.333,171.667 136.333,96.667 136.333,147.333 172,148 172,28.667 52,27.667 52.333,63.667 101.333,63.667'/></svg>
-            @else
-              <svg style="fill:red"><polygon points="101.356,135.319 52.359,135.794 52.372,171.795 172.359,169.634 171.203,50.306 135.544,51.318 136.034,101.981  61.314,27.702 27.315,62.366 "/> </svg>
-            @endif</td>
-          <td><a href="{{ url('/fav/') . $action['id'] }}"><i class="fa fa-star-o" aria-hidden="true"></i></a></td>
-        </tr>
-       </tbody>
-      @endforeach
-    </table>
-  </div>
+          <tbody class="cac40__tableBody">
+            <tr>
+              <td>{{ $action['nom'] }}</td>
+              <td>{{ $action['ISIN'] }}</td>
+              <td>{{ $action['ouverture'] }}</td>
+              <td>{{ $action['fermeture'] }}</td>
+              <td>{{ $action['bas'] }}</td>
+              <td>{{ $action['haut'] }}</td>
+              <td>{{ $action['variation'] }}
+                @if($action['variation']>= 0) <!-- fleche vers le haut -->
+                  <svg style="fill:green"><polygon points='28,137.333 62.333,171.667 136.333,96.667 136.333,147.333 172,148 172,28.667 52,27.667 52.333,63.667 101.333,63.667'/></svg>
+                @else
+                  <svg style="fill:red"><polygon points="101.356,135.319 52.359,135.794 52.372,171.795 172.359,169.634 171.203,50.306 135.544,51.318 136.034,101.981  61.314,27.702 27.315,62.366 "/> </svg>
+                @endif</td>
+              <td><a href="{{ url('/fav/') . $action['id'] }}"><i class="fa fa-star-o" aria-hidden="true"></i></a></td>
+            </tr>
+           </tbody>
+          @endforeach
+        </table>
+      </div>
 
-  <div class="cac80">
-  <h1>CAC 80</h1>
-    <table>
-    <thead>
-      <tr>
-        <td>Nom</td>
-        <td>ISIN</td>
-        <td>Ouverture</td>
-        <td>Fermeture</td>
-        <td>Min</td>
-        <td>Max</td>
-        <td>Variation</td>
-        <td>Fav</td>
-      </tr>
-      </thead>
-      @foreach ($actions as $action)
-      <tbody>
-        <tr>
-          <td>{{ $action['nom'] }}</td>
-          <td>{{ $action['ISIN'] }}</td>
-          <td>{{ $action['ouverture'] }}</td>
-          <td>{{ $action['fermeture'] }}</td>
-          <td>{{ $action['bas'] }}</td>
-          <td>{{ $action['haut'] }}</td>
-          <td>{{ $action['variation'] }}</td>
-        </tr>
-       </tbody>
-      @endforeach
-    </table>
-  </div>
-  </div>
+      <div class="cac80">
+        <h1>CAC 80</h1>
+        <table class="cac80__table">
+          <thead class="cac80__tableHead">
+            <tr>
+              <td>Nom</td>
+              <td>ISIN</td>
+              <td>Ouverture</td>
+              <td>Fermeture</td>
+              <td>Min</td>
+              <td>Max</td>
+              <td>Variation</td>
+              <td>Fav</td>
+            </tr>
+          </thead>
+          @foreach ($actions as $action)
+          <tbody class="cac80__tableBody">
+            <tr>
+              <td>{{ $action['nom'] }}</td>
+              <td>{{ $action['ISIN'] }}</td>
+              <td>{{ $action['ouverture'] }}</td>
+              <td>{{ $action['fermeture'] }}</td>
+              <td>{{ $action['bas'] }}</td>
+              <td>{{ $action['haut'] }}</td>
+              <td>{{ $action['variation'] }}</td>
+            </tr>
+           </tbody>
+          @endforeach
+        </table>
+      </div>
+    </div>
   </div>
   <div class="fav">
     <h1>MES FAVORIS</h1>
